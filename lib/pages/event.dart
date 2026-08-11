0d75837ed913af184e1d33c8de6f3a916139463b
Moustapha <moustaphamukwini@gmail.com>
2026-08-11 22:15:53 +0100

    create the event page

---
+++ lib/pages/event.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class EventPage extends StatelessWidget {
+  const EventPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text('Salon planing'),
+      ),
+      body: Center(
+        child: Text('Coming soon'),
+      ),
+    );
+  }
+}
