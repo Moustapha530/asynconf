fccf4c7cfd2b0e21bfedea2a1e04679e2a43357b
Moustapha <moustaphamukwini@gmail.com>
2026-08-12 11:49:57 +0100

    refactor event page to use stateful widget and display event list with images

--- lib/pages/event.dart
+++ lib/pages/event.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
 
 
-class EventPage extends StatelessWidget {
+class EventPage extends StatefulWidget {
   const EventPage({super.key});
+
+  @override
+  State<EventPage> createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
+
+  final events = [
+    {
+      'speaker': 'Lior chama',
+      'time': '13h - 13h30',
+      'subject': 'The code legacy',
+      'avatar': 'lior',
+    },
+    {
+      'speaker': 'Damien Cavaailles',
+      'time': '17h - 17h30',
+      'subject': 'git blame --no-offense',
+      'avatar': 'damien',
+    },
+    {
+      'speaker': 'Defend Intelligence',
+      'time': '18h - 18h30',
+      'subject': 'Discovering generative AI',
+      'avatar': 'defendintelligence',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +39,23 @@ class EventPage extends StatelessWidget {
         title: const Text('Salon planing'),
       ),
       body: Center(
-        child: Text('Coming soon'),
+        child: ListView.builder(
+            itemCount: events.length,
+            itemBuilder: (context, index) {
+              final eventData = events[index];
+              return Card(
+                child: ListTile(
+                  leading: Image.asset('assets/images/${eventData['avatar']}.jpg'),
+                  title: Text('${eventData['speaker']} (${eventData['time']})'),
+                  subtitle: Text(eventData['subject'] ?? ''),
+                  trailing: const Icon(Icons.info_outline),
+                  onTap: () {
+                    // Handle events tap
+                  },
+                ),
+              );
+            }
+          ),
       ),
     );
   }
